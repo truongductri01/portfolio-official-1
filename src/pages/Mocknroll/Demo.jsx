26158aad5ccc013b6925a8fdd5f706d8450b5992
truongductri01 <truongductri01@gmail.com>
2023-09-17 21:00:10 -0500

    feat: add more demo video

--- src/pages/Mocknroll/Demo.jsx
+++ src/pages/Mocknroll/Demo.jsx
@@ -17,6 +17,18 @@ function Demo() {
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowfullscreen
+                className="mt-[1rem]"
+            ></iframe>
+
+            <iframe
+                width="100%"
+                height="315"
+                src="https://www.youtube.com/embed/RcmpjME7sd4?si=kvCRBt7JbdWo6GPr"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+                className="mt-[1rem]"
             ></iframe>
         </div>
     );
