2fcbd2dca9a2423a4a530c75a448c85741ba2131
truongductri01 <truongductri01@gmail.com>
2023-12-12 14:55:49 -0500

    grammar fixed

--- src/pages/HomePage/Hero/Hero.jsx
+++ src/pages/HomePage/Hero/Hero.jsx
@@ -40,10 +40,10 @@ function Hero() {
             </p>
             <br />
             <p className="desc text-normal text-justify">
-                I have done two internship with <strong>Intuit</strong> in the
-                last 2 years which give me the understanding of not only how
+                I have done three internships with <strong>Intuit</strong> in the
+                last 3 years which give me the understanding of not only how
                 softwares are planned, developed, and deployed but also how
-                softwares make customers' life easier. I have worked closely
+                they make customers' life easier. I have worked closely
                 with 4 different developers and support over 10+ customers
                 directly by solving their issues and creating new features for
                 our product.
